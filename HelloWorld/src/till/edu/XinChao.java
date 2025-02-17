a99583eaf6dc90212a1834259f98a813c921187c
Pine2k2 <chemgiosaoquan@gmail.com>
2025-02-17 10:04:05 +0700

    Ví dụ đầu tiên
    chỉ để Demo

---
+++ HelloWorld/src/till/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package till.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.print("Toi lan dau hoc Java,va dang thu nghiem quan ly ma nguon voi git");
+	}
+
+}
